07789ca0ba02d8d893bba0a8c87c3ee0268cf678
Slava Pak <vpak@griddynamics.com>
2013-11-20 19:29:47 +0200

    add some configs to Scala port

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object ChickenBuild extends Build {
 
   lazy val root =
-    Project (id = "chicken",
-      base = file("."))
+    Project (id = "chicken", base = file(".")).
+      dependsOn(uri("git://github.com/sbt/sbt-assembly.git#0.10.1"))
 
 }
